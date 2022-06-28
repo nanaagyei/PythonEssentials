7afa73c13db1c3fcf55e9821b815a9fa4e305750
Prince <tuffourp@oregonstate.edu>
2022-06-28 14:48:04 -0700

    Exception and File IO

--- Exceptions_FileIO/exceptions_fileIO.py
+++ Exceptions_FileIO/exceptions_fileIO.py
@@ -24,10 +24,20 @@ def arithmagic():
     
     step_1 = input("Enter a 3-digit number where the first and last "
                                            "digits differ by 2 or more: ")
+    if len(step_1) != 3:
+        raise ValueError("The first number step_1 is not a 3-digit number.")
+    if abs(int(step_1[0]) - int(step_1[2])) < 2:
+        raise ValueError("The first number's first and last digits differ by less than 2.")
     step_2 = input("Enter the reverse of the first number, obtained "
                                               "by reading it backwards: ")
+    if step_2 != step_1[::-1]:
+        raise ValueError("The second number step_2 is not the reverse of the first number.")
     step_3 = input("Enter the positive difference of these numbers: ")
+    if int(step_3) != abs(int(step_1) - int(step_2)):
+        raise ValueError("The third number step_3 is not the positive difference of the first two numbers.")
     step_4 = input("Enter the reverse of the previous result: ")
+    if step_4 != step_3[::-1]:
+        raise ValueError("The fourth number step_4 is not the reverse of the third number.")
     print(str(step_3), "+", str(step_4), "= 1089 (ta-da!)")
 
 
@@ -44,9 +54,15 @@ def random_walk(max_iters=1e12):
     
     walk = 0
     directions = [1, -1]
-    for i in range(int(max_iters)):
-        walk += choice(directions)
-    return walk
+    try:
+        for i in range(int(max_iters)):
+            walk += choice(directions)
+    except KeyboardInterrupt:
+        print("Process interrupted at iteration", i)
+    else:
+        print("Process completed")
+    finally:
+        return walk
 
 
 # Problems 3 and 4: Write a 'ContentFilter' class.
